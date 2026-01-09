83d1fa06ba8a0a6f1b85c27f3b06527934ca0c75
storm-sandy <sanleosmile@gmail.com>
2026-01-09 13:48:42 +0530

    Implement service worker registration and update prompt
    Add service worker registration and update button functionality

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js');
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js");
+
+  navigator.serviceWorker.addEventListener("controllerchange", () => {
+    document.getElementById("updateBtn").style.display = "block";
+  });
 }
+
+document.getElementById("updateBtn").onclick = () => {
+  location.reload();
+};
