639ff0bf9aa997d262010991d041d337b4b3b287
storm-sandy <sanleosmile@gmail.com>
2026-01-11 16:29:17 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,42 +1,25 @@
 function payUPI() {
-  let upiUrl = "upi://pay?pa=imcsandeepsharma@oksbi&pn=Swami%20Samarth%20Travels&am=500&cu=INR";
-  window.location.href = upiUrl;
+  let upi =
+    "upi://pay?pa=imcsandeepsharma@oksbi&pn=Swami%20Samarth%20Travels&am=5&cu=INR";
+  window.location.href = upi;
 }
 
-function payRazorpay() {
-  var options = {
-    key: "rzp_test_xxxxx", // replace with live key
-    amount: 5, // ₹500
-    currency: "INR",
-    name: "Swami Samarth Travels",
-    description: "Trip Booking",
-    handler: function (response) {
-      alert("Payment Successful");
-      sendWhatsApp(response.razorpay_payment_id);
-    },
-    prefill: {
-      name: "Sandy Shar",
-      contact: "7666022883"
-    },
-    theme: {
-      color: "#3399cc"
-    }
-  };
+function confirmWhatsApp() {
+  let msg = `Hello,
+I have paid ₹5 via UPI.
+Please confirm my booking.
 
-  var rzp = new Razorpay(options);
-  rzp.open();
+Name: Sandy
+Date: 11jan2026
+Pickup Point: Nallasopara`;
+
+  window.open(
+    "https://wa.me/917666022883?text=" + encodeURIComponent(msg),
+    "_blank"
+  );
 }
 
-function payCOD() {
+function cod() {
   alert("Booking confirmed. Pay on pickup.");
-  sendWhatsApp("COD");
-  }
-
-function sendWhatsApp(paymentId) {
-  let msg = `Booking Confirmed
-Payment: ${paymentId}
-Thank you for choosing Swami Samarth Travels`;
-
-  let url = "https://wa.me/917666022883?text=" + encodeURIComponent(msg);
-  window.open(url, "_blank");
-            }
+  confirmWhatsApp();
+    }
